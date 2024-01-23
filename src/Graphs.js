a3b5e9b7191150f669edcfc790290baf0822cb9f
utkarshsorathia <utkarshsorathia.co20d1@scet.ac.in>
2024-01-23 23:01:16 +0530

    second change

--- src/Graphs.js
+++ src/Graphs.js
@@ -66,92 +66,106 @@ const Chart = () => {
   useEffect(() => {
     fetchData();
     fetchInfo();
-  });
+  }, []); // Added an empty dependency array to ensure useEffect runs only once.
+
   return (
-    <>
-      <div className="container d-flex justify-content-around">
-        <div className="container w-75 p-5 border rounded" >
-          <h1 className="header">Line Chart of {id}({timeline}).</h1>
-          <div className="button-main">
-            <button
-              className="button-time"
-              onClick={(e) => {
-                setTimeline("m15");
-              }}
-            >
-              15 Minutes
-            </button>
-            <button className="button-time" onClick={(e) => setTimeline("h1")}>
-              1 Hour
-            </button>
-            <button className="button-time" onClick={(e) => setTimeline("h6")}>
-              6 Hours
-            </button>
-            <button className="button-time" onClick={(e) => setTimeline("d1")}>
-              1 Day
-            </button>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-8 mx-auto">
+          <div className="container w-100 p-4 border rounded">
+            <h1 className="header">Line Chart of {id}({timeline}).</h1>
+            <div className="button-main">
+              <button
+                className="button-time"
+                onClick={(e) => {
+                  setTimeline("m15");
+                }}
+              >
+                15 Minutes
+              </button>
+              <button
+                className="button-time"
+                onClick={(e) => setTimeline("h1")}
+              >
+                1 Hour
+              </button>
+              <button
+                className="button-time"
+                onClick={(e) => setTimeline("h6")}
+              >
+                6 Hours
+              </button>
+              <button
+                className="button-time"
+                onClick={(e) => setTimeline("d1")}
+              >
+                1 Day
+              </button>
+            </div>
+            {loading ? (
+              <div>Loading...</div>
+            ) : (
+              <Line
+                data={chartdata}
+                options={{
+                  elements: {
+                    point: {
+                      radius: 0,
+                    },
+                  },
+                  plugins: {
+                    legend: {
+                      display: false,
+                    },
+                  },
+                }}
+              />
+            )}
+            <br />
+            <Link to="/" className="button">
+              Back
+            </Link>
           </div>
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <Line
-              data={chartdata}
-              options={{
-                elements: {
-                  point: {
-                    radius: 0,
-                  },
-                },
-                plugins: {
-                  legend: {
-                    display: false,
-                  },
-                },
-              }}
-            />
-          )}
-          <br />
-          <Link to="/" className="button">
-            Back
-          </Link>
         </div>
 
-        <div className="container w-25 p-5 mt-5 border rounded">
-          <h5>
-            <b>Name: </b>
-            {info.name}
-          </h5>
-          <h5>
-            <b>Symbol: </b>
-            {info.symbol}
-          </h5>
-          <h5>
-            <b>Market Cap. : </b>
-            {info.symbol}
-          </h5>
-          <h5>
-            <b>Current Price(USD): </b>
-            {parseFloat(info.priceUsd).toFixed(3)}
-          </h5>
-          <h5>
-            <b>Change % 24hr: </b>
-            {parseFloat(info.changePercent24Hr).toFixed(3)}
-          </h5>
-          <h5>
-            <b>Volume USD 24hr: </b>
-            {parseFloat(info.volumeUsd24Hr).toFixed(3)}
-          </h5>
-          <h5>
-            <b>More Information: </b>
-            <a
-              href={`https://www.blockchain.com/explorer/assets/${info.symbol}`}
-            >
-              Visit here.
-            </a>
-          </h5>
+        <div className="col-md-4 mt-4">
+          <div className="container w-100 p-4 border rounded">
+            <h5>
+              <b>Name: </b>
+              {info.name}
+            </h5>
+            <h5>
+              <b>Symbol: </b>
+              {info.symbol}
+            </h5>
+            <h5>
+              <b>Market Cap. : </b>
+              {info.marketCapUsd}
+            </h5>
+            <h5>
+              <b>Current Price(USD): </b>
+              {parseFloat(info.priceUsd).toFixed(3)}
+            </h5>
+            <h5>
+              <b>Change % 24hr: </b>
+              {parseFloat(info.changePercent24Hr).toFixed(3)}
+            </h5>
+            <h5>
+              <b>Volume USD 24hr: </b>
+              {parseFloat(info.volumeUsd24Hr).toFixed(3)}
+            </h5>
+            <h5>
+              <b>More Information: </b>
+              <a
+                href={`https://www.blockchain.com/explorer/assets/${info.symbol}`}
+              >
+                Visit here.
+              </a>
+            </h5>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
